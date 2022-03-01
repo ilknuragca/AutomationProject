2d239ec89ad062c4e4af8ef09e7103363ee05bb5
ilknur <aydinn.ilknuur@gmail.com>
2022-03-01 12:38:31 +0300

    end

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,24 +6,24 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-//        plugin = {
-//                "pretty",//This is used for more readable reports or outputs
-//                "html:target/default-cucumber-reports.html",//Use this reports
-//                "json:target/json-reports/cucumber.json",
-//                "junit:target/xml-report/cucumber.xml",
-//                //"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-//                "rerun:target/failedRerun.txt"
-//
-//        },
+        plugin = {
+               "pretty",//This is used for more readable reports or outputs
+               "html:target/default-cucumber-reports.html",//Use this reports
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml",
+               // "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "rerun:target/failedRerun.txt"
+
+        },
 
         features = "./src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@search_products2",
+        tags = "@vip",
         //tags="@register_user",
         //tags="@login_user_excel",
         //tags="@login_user_excel_negative",
        // tags="@verify_subscription_in_cart_page",
-        dryRun = true
+        dryRun = false
 )
 
 public class Runner {
--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.interactions.Actions;
 
 public class ReusableMethods {
     public static void jsScroll() {
-
+//yukardan aşşağı
         JavascriptExecutor je = (JavascriptExecutor) Driver.getDriver();
         je.executeScript("window.scrollTo(0,document.body.scrollHeight)");
 
@@ -18,5 +18,39 @@ public class ReusableMethods {
     public static void scrollToElement(WebElement element) {
         ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
     }
+    public void scrollDownWithJS() throws InterruptedException {
 
-}
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+
+        js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+        Thread.sleep(3000);
+
+    }
+
+    public static void clickWithJS(WebElement webElement) throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        Thread.sleep(3000);
+
+        js.executeScript("arguments[0].click();", webElement);
+
+    }
+
+    public static void clickElementByJS(WebElement element) {
+        JavascriptExecutor jsexecutor = ((JavascriptExecutor) Driver.getDriver());
+        jsexecutor.executeScript("arguments[0].click();", element);
+    }
+
+    public static void scrollIntoViewJS(WebElement element) {
+        JavascriptExecutor jsexecutor = ((JavascriptExecutor) Driver.getDriver());
+        jsexecutor.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    public static void scrollUpWithJS() throws InterruptedException {
+
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+
+        js.executeScript("window.scrollTo(document.body.scrollHeight,0)");
+        Thread.sleep(3000);
+
+
+    }}
